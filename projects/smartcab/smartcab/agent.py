c6a437e57454faf4ff1bcd8221fab9ba8477f9f7
Quang Vu <vhquang@gmail.com>
2017-11-26 19:34:09 -0800

    new data

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -45,14 +45,13 @@ class LearningAgent(Agent):
             self.epsilon = 0
             self.alpha = 0
         else:
-            # self.epsilon -= 0.05
-            # self.epsilon = self.alpha ** self.total_run
+            # self.epsilon -= 0.005
+            self.epsilon = self.alpha ** self.total_run
             # self.epsilon = 1 / (self.total_run ** 2 if self.total_run > 0 else 1)
             # self.epsilon = math.e ** (-self.alpha * self.total_run)
-            self.epsilon = math.cos(self.alpha * self.total_run)
+            # self.epsilon = math.cos(self.alpha * self.total_run)
             self.total_run += 1
             print 'qqq', self.total_run, self.alpha, self.epsilon
-
 
         return None
 
@@ -200,7 +199,7 @@ def run():
     #    * alpha   - continuous value for the learning rate, default is 0.5
     agent = env.create_agent(LearningAgent,
                              learning=True,
-                             alpha=0.6,
+                             alpha=0.95,
                              epsilon=0.95
                              )
 
