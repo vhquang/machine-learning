9180ef50c4c2ab9041e34ea801ff2d1c50d7d3ab
Quang Vu <vu@rallc.com>
2017-11-29 19:37:46 -0800

    re-optimize

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -46,13 +46,12 @@ class LearningAgent(Agent):
             self.epsilon = 0
             self.alpha = 0
         else:
-            self.epsilon -= 0.05
+            # self.epsilon -= 0.05
             # self.epsilon = 0.97 ** self.total_run
             # self.epsilon = 1 / (self.total_run ** 2 if self.total_run > 0 else 1)
-            # self.epsilon = math.e ** (-0.004 * self.total_run)
+            self.epsilon = math.e ** (-0.004 * self.total_run)
             # self.epsilon = math.cos(0.003 * self.total_run)
             self.total_run += 1
-            print 'qqq', self.total_run
 
         return None
 
@@ -224,7 +223,7 @@ def run():
     sim = Simulator(env, update_delay=0.000,
                     display=False,
                     log_metrics=True,
-                    optimized=False
+                    optimized=True
                     )
 
     ##############
@@ -251,7 +250,7 @@ def load_test_report(filename):
 
 
 if __name__ == '__main__':
-    for _ in range(1):
+    for _ in range(10):
         run()
-        # d = load_test_report('sim_improved-learning.csv').to_dict()
-        # print 'report:', '   '.join(['{}: {:.4f}'.format(k, v) for k, v in sorted(d.items())])
+        d = load_test_report('sim_improved-learning.csv').to_dict()
+        print 'report:', '   '.join(['{}: {:.4f}'.format(k, v) for k, v in sorted(d.items())])
