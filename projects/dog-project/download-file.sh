0ca1ad436084363dd92b4f3d2708a699b26672b0
Quang Vu <vu@rallc.com>
2017-12-30 16:45:28 -0800

    update script to download other transfer learning model

--- projects/dog-project/download-file.sh
+++ projects/dog-project/download-file.sh
@@ -2,4 +2,9 @@ wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/dogImages.zip
 unzip dogImages.zip  && rm dogImages.zip
 wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/lfw.zip
 unzip lfw.zip && rm lfw.zip && rm -rf __MACOSX
+
 wget -P ./bottleneck_features/ https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/DogVGG16Data.npz
+wget -P ./bottleneck_features/ https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/DogVGG19Data.npz
+wget -P ./bottleneck_features/ https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/DogResnet50Data.npz
+wget -P ./bottleneck_features/ https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/DogInceptionV3Data.npz
+wget -P ./bottleneck_features/ https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/DogXceptionData.npz
