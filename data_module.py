86c54a1b2c12b9cc836b5c91a2af0fc4ab612843
Quang Vu <vhquang@gmail.com>
2018-04-21 12:36:07 -0700

    for simplicity, make timestep of lenght 1, and ignore window size

--- data_module.py
+++ data_module.py
@@ -32,28 +32,28 @@ def split(data, ratio: float=0.9):
     m = round(len(data) * ratio)
     return data[:m], data[m:]
 
-def make_time_windows(data: tp.Iterable, timesteps: int, window_size: int) -> (np.ndarray, np.ndarray):
+def make_time_windows(data: tp.Iterable, timesteps: int) -> (np.ndarray, np.ndarray):
     """
     Split the data into sequence of training inputs and expected results.
     The result is the value right after the training input.
 
     Args:
         timesteps: a number of timestep (interval) in each training input.
-        window_size: a length of each timestep
 
     Ex:
-    >>> make_time_windows([1,2,3,4,5,6], timesteps=3, window_size=2)
+    >>> make_time_windows([0,1,2,3,4,5], timesteps=3)
+    train: [ [0,1,2], [1,2,3] ]
+    expected: [3,4]
     """
-    seq = [np.array(data[window_size * i: window_size * (i+1)])
-          for i in range(len(data) // window_size)]
-    train = [seq[i: i + timesteps] for i in range(len(seq) - timesteps)]
-    check = [seq[i + timesteps] for i in range(len(seq) - timesteps)]
-    assert len(train) == len(check)
-    return np.array(train), np.array(check)
+    n = len(data)
+    train = [data[i: i + timesteps] for i in range(n - timesteps - 1)]
+    expected = [data[i + timesteps] for i in range(n - timesteps - 1)]
+    assert len(train) == len(expected)
+    return np.array(train), np.array(expected)
 
 
 def main():
-    seq = make_time_windows([1,2,3,4,5,6], timesteps=3, window_size=2)
+    seq = make_time_windows(range(6), timesteps=3)
     print(seq[0], seq[1])
     print()
 
